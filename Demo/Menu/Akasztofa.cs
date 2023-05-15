02310742a5cbb04ddfc2880f17821a998c3bbb33
Karusz <112902245+Karusz@users.noreply.github.com>
2023-05-15 22:25:58 +0200

    Demo

---
+++ Demo/Menu/Akasztofa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menu
+{
+    public partial class Akasztofa : Form
+    {
+        Form opener;
+        public Akasztofa(Form parentForm)
+        {
+            InitializeComponent();
+            opener = parentForm;
+
+        }
+
+        private void Akasztofa_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Demo/Menu/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menu
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bj_Click(object sender, EventArgs e)
+        {
+            //Átmegy a BlackJack form-ba
+            Blackjack frm = new Blackjack(this);
+            frm.Show();
+            this.Visible = false;   //Eltünteti a menu-t
+        }
+
+        private void af_Click(object sender, EventArgs e)
+        {
+            //Átmegy az Akasztófa form-ba
+            Akasztofa frm = new Akasztofa(this);
+            frm.Show();
+            //this.Visible = false;
+        }
+
+        private void sm_Click(object sender, EventArgs e)
+        {
+            //Átmegy a Slot Machine form-ba
+            SlotMachine frm = new SlotMachine(this);
+            frm.Show();
+            //this.Visible = false;
+        }
+
+        private void quit_Click(object sender, EventArgs e)
+        {
+            Close(); //Bezárja a programot
+        }
+    }
+}
