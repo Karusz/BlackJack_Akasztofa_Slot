f5d0d4cf3185deb5fd12b106e288973d30569842
FabianLaszlo00 <fl20020107@gmail.com>
2023-05-22 20:09:52 +0200

    gomb

--- MenuGames/MenuGames/Akasztofa.Designer.cs
+++ MenuGames/MenuGames/Akasztofa.Designer.cs
@@ -29,24 +29,26 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Akasztofa));
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // panel1
+            // button1
             // 
-            this.panel1.Location = new System.Drawing.Point(357, 222);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(274, 67);
-            this.panel1.TabIndex = 0;
+            this.button1.Location = new System.Drawing.Point(481, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Akasztofa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Akasztofa";
             this.Text = "Akasztofa";
             this.ResumeLayout(false);
@@ -54,5 +56,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
